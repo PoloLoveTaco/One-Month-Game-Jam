37ba9e5f8e91011771b07705dac105f7e233b476
Polo <jules.trolle@epitech.eu>
2025-11-14 11:54:07 +0100

    [ADD] clickable button

--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -9,10 +9,10 @@ var current_choices: Array[UpgradeData] = []
 @onready var coins_label := $Panel/Header/Gold/GoldLabel
 @onready var upgrades_grid := $Panel/GridContainer
 
-
 @onready var shop_item_scene := preload("res://Scenes/Interfaces/shop.tscn")
 
 var player : Player
+
 
 func _ready() -> void:
 	get_tree().paused = true
@@ -21,8 +21,8 @@ func _ready() -> void:
 	
 	roll_shop(1, player.player_stat.base_stats["luck"])
 
+
 func roll_shop(wave: int, luck: float):
-	
 	var pool = possible_upgrades.filter(
 		func(u: UpgradeData) -> bool:
 			return can_show_upgrade(u, wave)
@@ -33,19 +33,21 @@ func roll_shop(wave: int, luck: float):
 	for i in range(choices_per_round):
 		var choice = pick_random_upgrade(pool, luck)
 		current_choices.append(choice)
-
-	update_ui()
 	
+	update_ui()
+
+
 func can_show_upgrade(u: UpgradeData, wave: int):
 	if u is WeaponData and player.inventory.weapons.size() >= player.inventory.max_weapons:
 		return false
 		
 	return true
 
+
 func pick_random_upgrade(pool: Array[UpgradeData], luck: float):
 	if pool.is_empty():
 		return null
-		
+	
 	var weights = []
 	for u in pool:
 		var base_weight = 1.0 / u.rarity
@@ -63,13 +65,14 @@ func pick_random_upgrade(pool: Array[UpgradeData], luck: float):
 			
 	return pool.back()
 
+
 func update_ui():
 	$Panel/GridContainer/ShopItem.set_item(current_choices[0])
 	$Panel/GridContainer/ShopItem2.set_item(current_choices[1])
 	$Panel/GridContainer/ShopItem3.set_item(current_choices[2])
 	$Panel/GridContainer/ShopItem4.set_item(current_choices[3])
 	$Panel/GridContainer/ShopItem5.set_item(current_choices[4])
-	
+
 
 func _on_reroll_button_pressed() -> void:
 	roll_shop(1, player.player_stat.base_stats["luck"])
@@ -82,20 +85,25 @@ func _on_next_wave_button_pressed() -> void:
 
 
 func _on_shop_item_pressed() -> void:
+	print("1")
 	player.player_stat.add_item(current_choices[0])
 
 
 func _on_shop_item_2_pressed() -> void:
+	print("2")
 	player.player_stat.add_item(current_choices[1])
 
 
 func _on_shop_item_3_pressed() -> void:
+	print("3")
 	player.player_stat.add_item(current_choices[2])
 
 
 func _on_shop_item_4_pressed() -> void:
+	print("4")
 	player.player_stat.add_item(current_choices[3])
 
 
 func _on_shop_item_5_pressed() -> void:
+	print("5")
 	player.player_stat.add_item(current_choices[4])
